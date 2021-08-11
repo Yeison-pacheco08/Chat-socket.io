6a86f613c46968b6650c8d17a9eb12ee8723bbd4
Yeison-pacheco08 <ypachecorangel15@correo.unicordoba.edu.co>
2021-08-11 13:49:07 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const http = require('./app');
 const port = 4000;
 
 http.listen(port, () => {
-    console.log('server is running on port', port);
+    console.log('Escuchando en el puerto', port);
 });
